6ac9230dda488467db0d3197739cda74bdcbda94
Aman-K-Ray <aman.k@talentsprint.com>
2025-04-30 18:36:56 +0530

    debug

--- openassessment/xblock/staff_area_mixin.py
+++ openassessment/xblock/staff_area_mixin.py
@@ -148,8 +148,7 @@ class StaffAreaMixin:
             else:
                 logger.info("#AMANK SITE CONFIG IN EDX ORA:%s", configuration_helpers.get_current_site_configuration_values())
                 context['is_enhanced_staff_grader_enabled'] = self.is_enhanced_staff_grader_enabled
-            base_url = configuration_helpers.get_value('ORA_GRADING_MICROFRONTEND_URL',
-                                default=getattr(settings, 'ORA_GRADING_MICROFRONTEND_URL', ''))
+            base_url = configuration_helpers.get_value('ORA_GRADING_MICROFRONTEND_URL','Test')
             context['enhanced_staff_grader_url'] = '{esg_url}/{block_id}'.format(
                 esg_url=base_url,
                 block_id=str(self.get_xblock_id())
