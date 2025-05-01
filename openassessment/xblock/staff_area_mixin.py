4fdde70a52d7d6ee77dba3afac8ef6106a4a410e
Aman-K-Ray <aman.k@talentsprint.com>
2025-05-01 14:50:40 +0530

    code cleanup

--- openassessment/xblock/staff_area_mixin.py
+++ openassessment/xblock/staff_area_mixin.py
@@ -148,7 +148,7 @@ class StaffAreaMixin:
             else:
                 context['is_enhanced_staff_grader_enabled'] = self.is_enhanced_staff_grader_enabled
             mfe_config = configuration_helpers.get_value('MFE_CONFIG', {})
-            base_url = mfe_config.get('ORA_GRADING_MICROFRONTEND_URL',default=getattr(settings, 'ORA_GRADING_MICROFRONTEND_URL', ''))
+            base_url = mfe_config.get('ORA_GRADING_MICROFRONTEND_URL', getattr(settings, 'ORA_GRADING_MICROFRONTEND_URL', ''))
             context['enhanced_staff_grader_url'] = '{esg_url}/{block_id}'.format(
                 esg_url=base_url,
                 block_id=str(self.get_xblock_id())
